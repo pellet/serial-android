0e1a4fdcdcb78c5fc9ac5ca5923096f46433ef5a
Ben Pettit <ben.pettit@smartcaptech.com>
2020-05-15 11:05:24 +1000

    fixed build of sample and added simple  build script.

--- build-libs.sh
+++ build-libs.sh
@@ -1,50 +1,7 @@
 #!/bin/bash
 
-ARGS=$(getopt -o "dca" -l "debug,clean,archive" -n "build-libs.sh" -- "$@");
+#build aars and sample project.
+./gradlew build
 
-eval set -- "$ARGS";
-
-cd libserial/src/main
-
-JNI_ARGS="NDK_LIBS_OUT=jniLibs"
-BUILD_TASK="assembleRelease"
-
-EXPORT_AAR=0
-
-while true; do
-  case "$1" in
-    -d|--debug)
-      shift
-      JNI_ARGS="$JNI_ARGS NDK_DEBUG=1"
-      BUILD_TASK="assembleDebug"
-      break;
-      ;;
-    -c|--clean)
-      shift
-      JNI_ARGS="$JNI_ARGS clean"
-      break;
-      ;;
-    -a|--archive)
-      shift
-      EXPORT_AAR=1
-      break;
-      ;;
-    --)
-      shift
-      break;
-      ;;
-  esac
-done
-
-# Remove '--'
-shift
-
-ndk-build $JNI_ARGS
-
-cd ../../..
-
-if [ "$EXPORT_AAR" = "1" ]; then
-  cd libserial
-  ../gradlew $BUILD_TASK
-  cd ..
-fi
+#copy aar to whereever you need it
+#cp libserial/build/outputs/aar/libserial-release.aar DEST
